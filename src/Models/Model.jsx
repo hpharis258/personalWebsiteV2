29f2d765ade00d4d2abb96d78c8cfef17482f9c2
Haroldas Varanauskas <hpharis258@gmail.com>
2024-09-15 20:09:03 +0100

    Laptop Rotation Fixed

--- src/Models/Model.jsx
+++ src/Models/Model.jsx
@@ -4,8 +4,8 @@ import { useGLTF } from "@react-three/drei";
 export function Model(props){
     const gltf = useGLTF('/macbook_pro_2021/scene.gltf');
     return(
-        <mesh scale={10}>
-            <primitive object={gltf.scene} />
+        <mesh scale={10} rotation-y={1.5} rotation-z={0.3}>
+            <primitive object={gltf.scene}/>
         </mesh>
     );
 }
